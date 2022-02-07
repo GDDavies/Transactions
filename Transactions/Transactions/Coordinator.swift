be8e17e1d342b1fac92161526e4998df420ea8cf
George Davies <georgedavies@Georges-MacBook-Pro.local>
2022-02-07 15:07:18 +0000

    Add coordinator

---
+++ Transactions/Transactions/Coordinator.swift
@@ -0,0 +1,27 @@
+//
+//  Coordinator.swift
+//  Transactions
+//
+//  Created by George Davies on 07/02/2022.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator {
+    func start()
+}
+
+final class MainCoordinator: Coordinator {
+    
+    private let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        
+    }
+    
+}
